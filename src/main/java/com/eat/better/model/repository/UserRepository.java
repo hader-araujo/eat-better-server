815e6544a36b3056a46e9927e6cb9384d2869b50
hader-araujo <haderaraujo@gmail.com>
2016-07-09 12:23:07 +0100

    Layers separeded per project

--- src/main/java/com/eat/better/model/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eat.better.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.eat.better.model.entity.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
-}
